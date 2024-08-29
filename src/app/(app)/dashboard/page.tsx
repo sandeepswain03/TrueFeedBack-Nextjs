b2cc9d0c52ef8f595981d1fa6d18c5b036b3020c
SandeepSwain <sandeepswain2004@gmail.com>
2024-08-29 17:25:19 +0530

    commit

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -178,9 +178,9 @@ function UserDashboard() {
       </Button>
       <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
         {messages.length > 0 ? (
-          messages.map((message, index) => (
+          messages.map((message) => (
             <MessageCard
-              key={message._id} 
+              key={message._id?.toString() ?? ''}
               message={message}
               onMessageDelete={handleDeleteMessage}
             />
